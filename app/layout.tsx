2674d7fdedf81b2f7c680373ab12a94f80bc1b84
takeoff4light-png <takeoff4light@gmail.com>
2026-02-23 17:36:17 +0200

    chore: add geist dependency

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,16 @@
-﻿import type { Metadata } from "next";
-import "./globals.css";
+﻿import "./globals.css";
+import type { ReactNode } from "react";
 import { Header } from "./components/Header";
 
-export const metadata: Metadata = {
-  title: "ThriveLineage",
-  description: "A sanctuary for thriving legacy.",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
         <Header />
-        <div className="container">{children}</div>
+        {children}
       </body>
     </html>
   );
 }
+
 
